4394464e9a7688e410ce5feb5474ebbd9e98bc5c
jacobadam <jacobanevitt@gmail.com>
2025-03-18 14:21:12 -0700

    add try catch block for better error handling

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,28 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = request.nextUrl;
-  const title = searchParams.get("title");
-  if (!title) {
+  try {
+    const { searchParams } = request.nextUrl;
+    const title = searchParams.get("title");
+    if (!title) {
+      return NextResponse.json(
+        { error: "Missing title parameter" },
+        { status: 400 }
+      );
+    }
+
+    const encodedTitle = encodeURIComponent(title);
+    const searchResults = await fetch(
+      `http://bechdeltest.com/api/v1/getMoviesByTitle?title=${encodedTitle}`
+    );
+
+    if (!searchResults.ok) {
+      return NextResponse.json(
+        { error: "Failed to fetch data" },
+        { status: searchResults.status }
+      );
+    }
+
+    const data = await searchResults.json();
+
+    if (data.length === 0) {
+      return NextResponse.json({
+        error: "Movie not found",
+        status: 404,
+      });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Search API error:", error);
     return NextResponse.json(
-      { error: "Missing title parameter" },
-      { status: 400 }
+      { error: "Internal server error" },
+      { status: 500 }
     );
   }
-
-  const encodedTitle = encodeURIComponent(title);
-  const searchResults = await fetch(
-    `http://bechdeltest.com/api/v1/getMoviesByTitle?title=${encodedTitle}`
-  );
-
-  if (!searchResults.ok) {
-    return NextResponse.json(
-      { error: "Failed to fetch data" },
-      { status: searchResults.status }
-    );
-  }
-
-  const data = await searchResults.json();
-
-  return NextResponse.json({ encodedTitle, data });
 }
