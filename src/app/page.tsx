87c74d96509e16e8be3e5f2928d7e1ab36b3fcbc
jacobadam <jacobanevitt@gmail.com>
2025-03-17 17:49:16 -0700

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="text-center mt-4 font-bold text-3xl">
-      Bechdel Test Checker
+    <div>
+      <h1 className="font-extrabold text-center text-3xl mt-4">
+        Bechdel Test Checker
+      </h1>
+      <ol className="text-center my-12">
+        <li>1. It has to have at least two [named] women in it</li>
+        <li>2. Who talk to each other</li>
+        <li>3. About something besides a man</li>
+      </ol>
     </div>
   );
 }
