6cccfc6869fb887841b6ba3de64b9c3e069ba136
jacobadam <jacobanevitt@gmail.com>
2025-03-18 15:00:01 -0700

    update client side api to make request to search route

--- src/utils/  bechdelTestApi.ts
+++ src/utils/  bechdelTestApi.ts
@@ -2,10 +2,9 @@ import { Movie } from "../types/movieTypes";
 
 export async function getMovieByTitle(title: string): Promise<Movie[]> {
   const response = await fetch(
-    `http://bechdeltest.com/api/v1/getMoviesByTitle?title=${encodeURIComponent(
-      title
-    )}`
+    `api/search/?title=${encodeURIComponent(title)}`
   );
+
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
