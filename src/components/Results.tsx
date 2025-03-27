ba8e719bc39086f4ac7a6e73efe5f21a21b814f8
jacobadam <jacobanevitt@gmail.com>
2025-03-27 13:33:28 -0700

    add map for movies and decode title

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import { Movie } from "@/types/movieTypes";
 import ParticlesBackground from "./ParticlesBackground";
+import { decode } from "html-entities";
 
 interface MovieDataProps {
   movieData: Movie[];
@@ -12,28 +12,27 @@ const Results: React.FC<MovieDataProps> = ({ movieData }) => {
   return (
     <div className="text-center mt-4">
       {movieData && movieData.length > 0 && (
-        <ul key={movieData[0].id}>
-          <li>{movieData[0].title}</li>
-          <li>{movieData[0].year}</li>
-          {movieData[0].rating === 3 ? (
-            <li>
-              <div className="flex justify-center items-center">
-                <ParticlesBackground />
-                <Image src="/true.png" width={100} height={100} alt="Pass" />
-              </div>
+        <ul>
+          {movieData.map((movie) => (
+            <li key={movie.id}>
+              <div>{decode(movie.title)}</div>
+              <div>{movie.year}</div>
+              {movie.rating === 3 ? (
+                <div>
+                  <div className="flex justify-center items-center">
+                    <ParticlesBackground />
+                    TRUE!!!
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="flex justify-center items-center">
+                    FALSE!!!
+                  </div>
+                </div>
+              )}
             </li>
-          ) : (
-            <li>
-              <div className="flex justify-center items-center">
-                <Image
-                  src="/false.png"
-                  width={100}
-                  height={100}
-                  alt="NotPass"
-                />
-              </div>
-            </li>
-          )}
+          ))}
         </ul>
       )}
     </div>
