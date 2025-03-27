09c1bf1e9809539a2238d4bad6e0f008ac3786cb
jacobadam <jacobanevitt@gmail.com>
2025-03-27 12:51:49 -0700

    remove logs

--- src/utils/  bechdelTestApi.ts
+++ src/utils/  bechdelTestApi.ts
@@ -1,10 +1,8 @@
 import { Movie } from "../types/movieTypes";
 
 export async function getMovieByTitle(title: string): Promise<Movie[]> {
-  console.log("fetch");
   const titleWithoutThe = title.replace(/^(the\s+)/i, "");
 
-  console.log(titleWithoutThe);
   const response = await fetch(
     `api/search/?title=${encodeURIComponent(titleWithoutThe)}`
   );
