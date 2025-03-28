8ebf244238416ed4b1fdce104f346ee4303dcd0e
jacobadam <jacobanevitt@gmail.com>
2025-03-28 16:46:49 -0700

    remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 import SearchBar from "@/components/SearchBar";
 import Results from "@/components/Results";
