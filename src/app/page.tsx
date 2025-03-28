b0ddee05c3f97d18339a90e5afeb5a671147aaa3
jacobadam <jacobanevitt@gmail.com>
2025-03-28 16:42:09 -0700

    add tool tip for about and minor styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,23 +21,37 @@ const Home: React.FC = () => {
   return (
     <div className="w-screen">
       <div className="flex flex-row mx-auto w-auto items-center justify-between ">
-        <Image
-          src="/bt-logo-black.gif"
-          width={80}
-          height={80}
-          alt="Picture of the author"
-        />
-        <h1 className="relative flex-1 font-extrabold text-center text-lg sm:text-3xl mt-4 justify-center">
+        <div className="mt-2">
+          <Image
+            src="/bt-logo-black.gif"
+            width={80}
+            height={80}
+            alt="Picture of the author"
+          />
+        </div>
+        <h1 className="relative flex-1 font-extrabold text-center text-lg sm:text-3xl mt-2 justify-center">
           Bechdel Test Checker
         </h1>
-        <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-4 mr-4 text-xs sm:text-base"
-          href="https://bechdeltest.com/"
-        >
-          About
-        </Link>
+
+        <div className="mt-4 mr-4 text-xs sm:text-base">
+          <div className="group relative">
+            <button>
+              <b>About</b>
+            </button>
+
+            <div className="absolute right-full top-full mb-2 hidden group-hover:block bg-black text-white text-sm rounded p-4 min-w-96 z-10 transition-opacity duration-300 opacity-0 group-hover:opacity-100 border border-white whitespace-normal text-center">
+              The Bechdel-Wallace Test, often known as the Bechdel Test, is a
+              basic measure assessing female representation in films. It
+              stipulates three key criteria: firstly, the film must feature at
+              least two named female characters, secondly, these characters must
+              engage in conversation with each other, and thirdly, their
+              dialogue must center on a topic other than men. This simple test,
+              originating from Alison Bechdel&apos;s 1985 comic strip
+              &quot;Dykes to Watch Out For,&quot; highlights the frequent lack
+              of meaningful female interaction in cinematic narratives.
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="flex flex-col items-center content-center my-12">
