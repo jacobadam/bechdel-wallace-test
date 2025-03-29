14596b9807f9996a51842f06c04ed131929b4dbf
jacobadam <jacobanevitt@gmail.com>
2025-03-29 11:58:50 -0700

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="mt-auto">
+      <div className="fixed bottom-2 left-2 text-xs text-white">
+        &copy; 2025 Jacob Nevitt
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
