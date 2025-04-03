1666c5289666d287e31d688371079941e37170b3
jacobadam <jacobanevitt@gmail.com>
2025-04-03 12:05:27 -0600

    add event handler for closing modal when clicking outside it

--- src/components/AboutInformation.tsx
+++ src/components/AboutInformation.tsx
@@ -18,6 +18,12 @@ const AboutInformation: React.FC = () => {
     setIsModalOpen(false);
   };
 
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      handleClose();
+    }
+  };
+
   return (
     <>
       <button className="relative group font-extrabold" onClick={handleClick}>
@@ -30,7 +36,10 @@ const AboutInformation: React.FC = () => {
       </button>
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50">
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black/80 z-50"
+          onClick={handleOverlayClick}
+        >
           <div className="bg-black/100 text-white text-sm rounded p-4 min-w-96 max-w-lg border border-white whitespace-normal text-center relative m-4">
             <button className="absolute top-2 right-2" onClick={handleClose}>
               X
