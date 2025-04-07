03ad08c974c6c9f977459f758422a2dc98d4469e
jacobadam <jacobanevitt@gmail.com>
2025-04-07 14:11:15 -0600

    update dims

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       {
         url: "/black-logo.png",
         width: 1080,
-        height: 1680,
+        height: 1080,
         alt: "Bechdel-Wallace Test Logo",
       },
     ],
