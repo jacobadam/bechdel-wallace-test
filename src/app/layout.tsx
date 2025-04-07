b5aeef849c74bd7c6a201e4533e9db2f0fe5de5c
jacobadam <jacobanevitt@gmail.com>
2025-04-07 15:02:41 -0600

    remove head element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,24 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-black">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
+    <html lang="en/">
       <body className={`${roboto.variable} ${robotoMono.variable} antialiased`}>
         {children}
       </body>
