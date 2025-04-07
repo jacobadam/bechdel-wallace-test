07ed045941ec6bc2d1e336a20b5d6630a531d0b7
jacobadam <jacobanevitt@gmail.com>
2025-04-07 10:31:23 -0600

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,35 @@ export const metadata: Metadata = {
   title: "Bechdel-Wallace Test Checker",
   description:
     "Check if a movie passes the Bechdel-Wallace Test. Enter a movie title and see the results.",
+  openGraph: {
+    title: "Bechdel-Wallace Test Checker",
+    description:
+      "Check if a movie passes the Bechdel-Wallace Test. Enter a movie title and see the results.",
+    url: "https://bechdeltest.vercel.app/",
+    siteName: "Bechdel-Wallace Test Checker",
+    images: [
+      {
+        url: "/black-logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Bechdel-Wallace Test Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bechdel-Wallace Test Checker",
+    description:
+      "Check if a movie passes the Bechdel-Wallace Test. Enter a movie title and see the results.",
+    images: ["/black-logo.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -25,6 +54,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${roboto.variable} ${robotoMono.variable} antialiased`}>
         {children}
       </body>
