71665607fc5be34292b13d9614dbaf50e6f43f5d
jacobadam <jacobanevitt@gmail.com>
2025-04-08 12:04:07 -0600

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -78,6 +79,7 @@ export default function RootLayout({
       </head>
       <body className={`${roboto.variable} ${robotoMono.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
